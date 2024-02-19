b0222c1dd5bd9facb0e24477e5b869e246ed58f2
aperfer126 <alberto.perez@iescelia.org>
2024-02-19 14:12:54 +0100

    cuarto commit

--- Calculadora.java
+++ Calculadora.java
@@ -2,4 +2,9 @@ public class Calculadora {
     public int suma(int num1, int num2) {
         return num1 + num2;
     }
+
+    public int resta(int num1, int num2) {
+        //modulo resta
+        return num1 - num2;
+    }
 }
