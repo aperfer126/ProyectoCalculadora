00af726eb0939f3265e14a94feca7dbaa27e1b42
aperfer126 <alberto.perez@iescelia.org>
2024-02-19 14:14:45 +0100

    Quinto commit

--- Calculadora.java
+++ Calculadora.java
@@ -2,4 +2,9 @@ public class Calculadora {
     public int suma(int num1, int num2) {
         return num1 + num2;
     }
+
+    
+    public int multiplicacion(int num1, int num2) {
+        return num1 * num2;
+    }
 }
