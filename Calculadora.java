224c68500398ca0b306e89eb0fe84fb9ff83fc68
aperfer126 <alberto.perez@iescelia.org>
2024-02-19 14:16:22 +0100

    Sexta commit

--- Calculadora.java
+++ Calculadora.java
@@ -7,4 +7,11 @@ public class Calculadora {
     public int multiplicacion(int num1, int num2) {
         return num1 * num2;
     }
+
+    public double division(int num1, int num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("El divisor no puede ser cero");
+        }
+        return (double) num1 / num2;
+    }
 }
